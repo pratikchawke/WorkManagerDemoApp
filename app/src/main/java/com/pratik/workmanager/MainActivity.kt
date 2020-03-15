5c3c36bf031a28cb0332af5982556e880ce237ad
Pratik Chawke <pratik.chawke@sugarboxnetworks.com>
2020-03-15 23:23:15 +0530

    Print log line with OneTimeWorkRequest and PeriodicWorkRequest

--- app/src/main/java/com/pratik/workmanager/MainActivity.kt
+++ app/src/main/java/com/pratik/workmanager/MainActivity.kt
@@ -2,11 +2,27 @@ package com.pratik.workmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.work.OneTimeWorkRequest
+import androidx.work.PeriodicWorkRequest
+import androidx.work.WorkManager
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun performSingleTask(view: View) {
+        val singleTask = OneTimeWorkRequest.Builder(OneTimeTaskWorker::class.java).build()
+        WorkManager.getInstance(applicationContext).enqueue(singleTask)
+    }
+    fun performPeriodicTask(view: View) {
+        val periodicTask = PeriodicWorkRequest.Builder(PeriodicTaskWorker::class.java,16,TimeUnit.MINUTES)
+            .build()
+        WorkManager.getInstance(applicationContext).enqueue(periodicTask)
     }
 }
--- app/src/main/java/com/pratik/workmanager/OneTimeTaskWorker.kt
+++ app/src/main/java/com/pratik/workmanager/OneTimeTaskWorker.kt
@@ -5,12 +5,16 @@ import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 
-class PeriodicTaskWorker(context: Context, workerParams: WorkerParameters) :
+class OneTimeTaskWorker(context: Context, workerParams: WorkerParameters) :
     Worker(context, workerParams) {
-    val TAG = PeriodicTaskWorker::class.java.simpleName
+
+val TAG = OneTimeTaskWorker::class.java.simpleName
     override fun doWork(): Result {
 
-        Log.d(TAG, "This is Periodic Worker Thread !!!")
+        Log.d(TAG,"This is Worker Thread !!!")
+
         return Result.success()
     }
+
+
 }
